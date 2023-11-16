09092c94628637ad044e4df01c45b4404bb1aeef
Agustin Fornasiero <agustin_fornasiero@hotmail.com>
2023-11-16 17:27:09 -0300

    Add docker compose Add docker files Remove tracked files by mistake Add .gitignore Change way of loading environment variables

--- unilingo-frontend/src/app/youtubeservice.service.ts
+++ unilingo-frontend/src/app/youtubeservice.service.ts
@@ -1,28 +1,28 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class YoutubeService {
-  private baseUrl = 'https://localhost:7207/api/YouTube';
 
   constructor(private http: HttpClient) {}
 
   getChannelVideos(): Observable<any> {
-    return this.http.get(`${this.baseUrl}`);
+    return this.http.get(`${environment.baseUrl}`);
   }
 
   getVideoTitle(videoURL: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/${encodeURIComponent(videoURL)}`);
+    return this.http.get(`${environment.baseUrl}/${encodeURIComponent(videoURL)}`);
   }
 
   getMostRecentVideo(): Observable<any> {
-    return this.http.get(`${this.baseUrl}/GetMostRecentVideo`);
+    return this.http.get(`${environment.baseUrl}/GetMostRecentVideo`);
   }
 
   getVideoInformation(videoURL: string): Observable<any> {
-    return this.http.get(`${this.baseUrl}/viewCount?videoURL=${encodeURIComponent(videoURL)}`);
+    return this.http.get(`${environment.baseUrl}/viewCount?videoURL=${encodeURIComponent(videoURL)}`);
   }
 }
