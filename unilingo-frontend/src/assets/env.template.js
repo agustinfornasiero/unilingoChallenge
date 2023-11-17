18bb8ad57e0cbdbaf5397eef16d05ae20e3c755b
Agustin Fornasiero <agustin_fornasiero@hotmail.com>
2023-11-16 21:17:46 -0300

    Changes to env variables for frontend

--- unilingo-frontend/src/assets/env.template.js
+++ unilingo-frontend/src/assets/env.template.js
@@ -2,6 +2,6 @@
     window.env = window.env || {};
   
     // Environment variables
-    window["env"]["apiUrl"] = "${APP_API_BASE_URL}";
-    window["env"]["debug"] = "${DEBUG}";
+    window["env"]["apiUrl"] = "${UI_API_BASE_URL}";
+    window["env"]["debug"] = "${UI_DEBUG}";
   })(this);
